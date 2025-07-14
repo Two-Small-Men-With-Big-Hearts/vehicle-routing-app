e440969554dea747ff9e87bb4b852afa48e61dc1
TanishqM1 <113214169+TanishqM1@users.noreply.github.com>
2025-07-14 08:43:24 -0700

    added login/logout functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,15 +34,20 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <header className="flex justify-center items-center p-4 gap-6 h-18 mt-35 pt-15">
             <SignedOut>
-              <SignInButton />
+              <SignInButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 w-25 sm:h-12 px-4 sm:px-5 cursor-pointer bg-black hover:bg-gray-800">
+                  Sign In
+                </button>
+                </SignInButton>
               <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 w-25 sm:h-12 px-4 sm:px-5 cursor-pointer bg-black hover:bg-gray-800">
                   Sign Up
                 </button>
               </SignUpButton>
             </SignedOut>
+            
             <SignedIn>
               <UserButton />
             </SignedIn>
